e2b8f722463fddbe69ed9f06e77063a265ff3d32
Lucar-aster <luca.r@astercucine.it>
2025-03-18 15:02:05 +0100

    Delete mio-sito-svelte/mio-sito-svelte directory

--- mio-sito-svelte/mio-sito-svelte/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from "vite";
-import svelte from "@sveltejs/vite-plugin-svelte";
-
-export default defineConfig({
-  plugins: [svelte()],
-  base: "/mio-sito-svelte/",  // Cambia con il nome del tuo repository
-});
